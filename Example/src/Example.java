51ccc7f18eaf4188ef28c40142c32e56905504d7
Rahul Mohite <rahulmohite.sid@gmail.com>
2022-01-25 10:41:04 +0000

    a

---
+++ Example/src/Example.java
@@ -0,0 +1,24 @@
+import java.io.BufferedInputStream;
+
+public class Example {
+    public static void main(String[] args) {
+
+
+        //BufferedInputStream bufferedInputStream = new BufferedInputStream(System.in);
+
+        try{
+            float a = 2/0;
+
+
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+
+
+    }
+
+    public static void input(){
+
+
+    }
+}
---
+++ Example/src/ManualException.java
@@ -0,0 +1,9 @@
+public class ManualException extends RuntimeException {
+
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
